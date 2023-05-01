8b1a7fb2bbeab8c782c8a4f33ffb917c9a827576
unikzforce <h.bakhtiary.z@gmail.com>
2023-05-01 19:38:52 +0200

    fix style

--- examples/c/minimal.bpf.c
+++ examples/c/minimal.bpf.c
@@ -12,11 +12,10 @@ unsigned long long dev;
 unsigned long long ino;
 
 SEC("tp/syscalls/sys_enter_write")
-int handle_tp(void *ctx)
-{
-    struct bpf_pidns_info ns;
+int handle_tp(void *ctx) {
+	struct bpf_pidns_info ns;
 
-    bpf_get_ns_current_pid_tgid(dev, ino, &ns, sizeof(ns));
+	bpf_get_ns_current_pid_tgid(dev, ino, &ns, sizeof(ns));
 	if (ns.pid != my_pid)
 		return 0;
 
