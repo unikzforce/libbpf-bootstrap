63bf31d60f426acc6ee85be47011b2c431a20af8
hossein bakhtiari <hossein.bakhtiari@revolut.com>
2023-08-18 19:59:59 +0200

    add unsafe mechanism instead of mutex

--- examples/rust/xdp_switch/src/main.rs
+++ examples/rust/xdp_switch/src/main.rs
@@ -1,6 +1,6 @@
 use std::sync::atomic::{AtomicBool, Ordering};
 
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 use std::mem;
 use std::{thread, time};
 use network_interface::NetworkInterface;
@@ -23,6 +23,7 @@ use xdp_switch::*;
 use chrono::Utc;
 use libbpf_rs::MapFlags;
 use moka::notification::RemovalCause;
+use unsafe_send_sync::UnsafeSend;
 
 const ETH_ALEN: usize = 6;
 
@@ -86,53 +87,48 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let skel_builder = XdpSwitchSkelBuilder::default();
     let open_skel = skel_builder.open()?;
-    let skel = Arc::new(Mutex::new(open_skel.load()?));
+    let skel = Arc::new(UnsafeSend::new(open_skel.load()?));
 
     let skel_for_eviction_clone = Arc::clone(&skel);
     let eviction_listener = move |k: Arc<mac_address>, v: iface_index, _: RemovalCause| {
-        if let Ok(mut skel_guard) = skel_for_eviction_clone.lock() {
-            let skel_ref = &mut *skel_guard;
-            let maps = skel_ref.maps();
-            let kernel_mac_table = maps.mac_table();
-            let existing_kernel_entry = kernel_mac_table.lookup(&k.mac, MapFlags::ANY);
+        let maps = skel_for_eviction_clone.as_ref().maps();
+        let kernel_mac_table = maps.mac_table();
+        let existing_kernel_entry = kernel_mac_table.lookup(&k.mac, MapFlags::ANY);
 
-            match existing_kernel_entry {
-                Ok(Some(data)) => {
-                    // The data is available, now we can try to convert it to iface_index struct
-                    if data.len() == std::mem::size_of::<iface_index>() {
-                        let iface_index_data = unsafe { &*(data.as_ptr() as *const iface_index) };
+        match existing_kernel_entry {
+            Ok(Some(data)) => {
+                // The data is available, now we can try to convert it to iface_index struct
+                if data.len() == std::mem::size_of::<iface_index>() {
+                    let iface_index_data = unsafe { &*(data.as_ptr() as *const iface_index) };
 
-                        let timestamp_seconds = iface_index_data.timestamp / 1_000_000_000; // Convert timestamp to seconds
+                    let timestamp_seconds = iface_index_data.timestamp / 1_000_000_000; // Convert timestamp to seconds
 
-                        let current_time = Utc::now().timestamp();
-                        let time_difference = current_time - timestamp_seconds as i64;
+                    let current_time = Utc::now().timestamp();
+                    let time_difference = current_time - timestamp_seconds as i64;
 
-                        if time_difference < 500 {
-                            sender.send(mac_address_iface_entry {
-                                mac: *k.clone().as_ref(),
-                                iface: v.clone(),
-                            }).expect("oeuoeu");
-                        } else {
-                            let _ = kernel_mac_table.delete(&k.mac);
-                        }
+                    if time_difference < 500 {
+                        sender.send(mac_address_iface_entry {
+                            mac: *k.clone().as_ref(),
+                            iface: v.clone(),
+                        }).expect("oeuoeu");
                     } else {
-                        eprintln!("Invalid data size for iface_index");
+                        let _ = kernel_mac_table.delete(&k.mac);
                     }
-                }
-                Ok(None) => {
-                    println!("No entry found for the given MAC address");
-                }
-                Err(err) => {
-                    eprintln!("Error while looking up the MAC address: {:?}", err);
+                } else {
+                    eprintln!("Invalid data size for iface_index");
                 }
             }
-        } else {
-            eprintln!("Failed to get mutable access to skel in eviction_listener");
+            Ok(None) => {
+                println!("No entry found for the given MAC address");
+            }
+            Err(err) => {
+                eprintln!("Error while looking up the MAC address: {:?}", err);
+            }
         }
     };
 
-    let user_mac_table: Arc<Mutex<Cache<mac_address, iface_index>>> = Arc::new(
-        Mutex::new(
+    let user_mac_table: Arc<UnsafeSend<Cache<mac_address, iface_index>>> = Arc::new(
+        UnsafeSend::new(
             Cache::builder()
                 .eviction_listener(eviction_listener)
                 .build(),
@@ -142,39 +138,35 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let user_mac_table_clone = Arc::clone(&user_mac_table);
     let receiver_thread = thread::spawn(move || {
         while let Ok(item) = receiver.recv() {
-            let _ = &(*user_mac_table_clone.lock().unwrap()).insert(item.mac, item.iface);
+            let _ = user_mac_table_clone.as_ref().insert(item.mac, item.iface);
         }
     });
 
     receiver_thread.join().expect("Failed to join the receiver thread");
 
 
-    let skel_for_attach_clone = Arc::clone(&skel);
+    let mut skel_for_attach_clone = Arc::clone(&skel);
     filtered_network_interfaces.iter().try_for_each(move |iface| -> Result<(), Box<dyn std::error::Error>> {
-        if let Ok(mut skel_guard) = skel_for_attach_clone.lock() {
-            let skel_ref = &mut *skel_guard;
-            let progs_mut = &mut skel_ref.progs_mut();
-            let _link = progs_mut.xdp_switch().attach_xdp(iface.index as i32)?;
+        if let Some(skel_for_attach_inner) = Arc::get_mut(&mut skel_for_attach_clone) {
+            let _link = skel_for_attach_inner.progs_mut().xdp_switch().attach_xdp(iface.index as i32)?;
         } else {
-            eprintln!("Failed to get mutable access to skel in main");
+            eprintln!("Failed to obtain mutable reference to skel");
         }
         Ok(())
     })?;
 
     let mut builder = libbpf_rs::RingBufferBuilder::new();
     let skel_for_new_discoveries_clone = Arc::clone(&skel);
+
+    let maps = skel_for_new_discoveries_clone.as_ref().maps();
+
     let user_mac_table_clone_2 = Arc::clone(&user_mac_table);
-    if let Ok(mut skel_guard) = skel_for_new_discoveries_clone.lock() {
-        let skel_ref = &mut *skel_guard;
-        let maps = skel_ref.maps();
-        builder
-            .add(maps.new_discovered_entries_rb(), move |data| {
-                new_discovered_entry_handler(data, &(*user_mac_table_clone_2.lock().unwrap()))
-            })
-            .unwrap();
-    } else {
-        eprintln!("Failed to get mutable access to skel in main");
-    }
+    builder
+        .add(maps.new_discovered_entries_rb(), move |data| {
+            new_discovered_entry_handler(data, user_mac_table_clone_2.as_ref().clone().unwrap())
+        })
+        .unwrap();
+
 
 
     let running = Arc::new(AtomicBool::new(true));
@@ -191,7 +183,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-fn new_discovered_entry_handler(data: &[u8], user_mac_table: &Cache<mac_address, iface_index>) -> ::std::os::raw::c_int {
+fn new_discovered_entry_handler(data: &[u8], user_mac_table: Cache<mac_address, iface_index>) -> ::std::os::raw::c_int {
     if data.len() != mem::size_of::<mac_address_iface_entry>() {
         eprintln!(
             "Invalid size {} != {}",
