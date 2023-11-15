f51a1c171448f2778540cc5b3c0353e5a763d463
unikzforce <h.bakhtiary.z@gmail.com>
2023-11-16 00:11:45 +0100

    try to fix unknown unicast flooding error

--- examples/rust/xdp_switch/src/bpf/unknown_unicast_flooding.bpf.c
+++ examples/rust/xdp_switch/src/bpf/unknown_unicast_flooding.bpf.c
@@ -14,6 +14,12 @@ long unknown_unicast_flooding(struct __sk_buff *skb)
 
 	__u64 current_time = bpf_ktime_get_ns();
 
+	struct ethhdr *eth = (void *)(long)skb->data;
+
+	// Additional check after the adjustment
+	if ((void *)(eth + 1) > (void *)(long)skb->data_end)
+		return BPF_DROP;
+
 	int ingress_ifindex = skb->ingress_ifindex;
 
 	if (number_of_interfaces >= 19) {
@@ -28,24 +34,4 @@ long unknown_unicast_flooding(struct __sk_buff *skb)
 
 	return BPF_DROP;
 
-//	bpf_printk(
-//		"----------------------------------------------------------------------------------------------------");
-//	// we can use current_time as something like a unique identifier for packet
-//
-//
-//	struct ethhdr *eth = (void *)(long)ctx->data;
-//
-//	// Additional check after the adjustment
-//	if ((void *)(eth + 1) > (void *)(long)ctx->data_end)
-//		return XDP_ABORTED;
-//
-//	bpf_printk(
-//		"id = %llx, interface = %d, Packet received, source MAC: %02x:%02x:%02x:%02x:%02x:%02x\n",
-//		current_time, ctx->ingress_ifindex, eth->h_source[0], eth->h_source[1],
-//		eth->h_source[2], eth->h_source[3], eth->h_source[4], eth->h_source[5]);
-//
-//	bpf_printk(
-//		"id = %llx, interface = %d, Packet received, dest MAC: %02x:%02x:%02x:%02x:%02x:%02x\n",
-//		current_time, ctx->ingress_ifindex, eth->h_dest[0], eth->h_dest[1], eth->h_dest[2],
-//		eth->h_dest[3], eth->h_dest[4], eth->h_dest[5]);
 }
