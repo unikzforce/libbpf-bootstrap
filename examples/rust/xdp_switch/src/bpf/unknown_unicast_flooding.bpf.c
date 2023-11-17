11a1cf4c44e06f488d890a83274775e8ee15900e
unikzforce <h.bakhtiary.z@gmail.com>
2023-11-17 01:56:23 +0100

    improve logging in TC

--- examples/rust/xdp_switch/src/bpf/unknown_unicast_flooding.bpf.c
+++ examples/rust/xdp_switch/src/bpf/unknown_unicast_flooding.bpf.c
@@ -3,7 +3,6 @@
 #include <bpf/bpf_helpers.h>
 #include <linux/pkt_cls.h>
 
-
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
 
 __u32 interfaces[20] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
@@ -26,11 +25,18 @@ int unknown_unicast_flooding(struct __sk_buff *skb)
 		current_time, skb->ingress_ifindex, eth->h_source[0], eth->h_source[1],
 		eth->h_source[2], eth->h_source[3], eth->h_source[4], eth->h_source[5]);
 
+	bpf_printk(
+		"///////////// id = %llx, interface = %d, Packet received, dest MAC: %02x:%02x:%02x:%02x:%02x:%02x\n",
+		current_time, skb->ingress_ifindex, eth->h_dest[0], eth->h_dest[1], eth->h_dest[2],
+		eth->h_dest[3], eth->h_dest[4], eth->h_dest[5]);
+
 	int ingress_ifindex = skb->ingress_ifindex;
 
 	if (number_of_interfaces >= 19) {
 		return BPF_DROP;
 	}
+
+	bpf_printk("///////////// id = %llx, interface = %d, start to multicast\n", current_time, skb->ingress_ifindex);
 
 	for (unsigned int iface_index = 0; iface_index < number_of_interfaces; iface_index++) {
 		if (iface_index >= 20) {
@@ -39,8 +45,8 @@ int unknown_unicast_flooding(struct __sk_buff *skb)
 
 		if (interfaces[iface_index] != ingress_ifindex) {
 			bpf_clone_redirect(skb, interfaces[iface_index], 0);
-			bpf_printk("///////////// id = %llx, redirecting to %d \n", current_time,
-				   interfaces[iface_index]);
+			bpf_printk("///////////// id = %llx, multicast: redirection to %d \n",
+				   current_time, interfaces[iface_index]);
 		}
 	}
 
